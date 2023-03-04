63db7f2ab93c8e7af174cc445cd3fb29129c5fc2
mfahadarif85 <mfahadarif@gmail.com>
2023-03-05 00:54:18 +0500

    prod-prev-card: added fonts

--- app/prod-prev-card/page.tsx
+++ app/prod-prev-card/page.tsx
@@ -9,21 +9,26 @@ const Home = () => {
         <div className='basis-1/2'>
           <Image src={deskProductImg} alt='product' />
         </div>
-        <div className='ml-4 basis-1/2'>
-          <h3 className='mt-10 font-extralight'>Preview</h3>
-          <h1 className='mt-6 text-5xl font-extrabold'>
+        <div className='mx-8 basis-1/2'>
+          <h3 className='mt-8 text-xl uppercase tracking-[5px] text-gray-400'>
+            Perfume
+          </h3>
+          <h1 className='mt-6 font-fraunces text-5xl'>
             Gabrielle Essence Eau De Parfum
           </h1>
-          <p className='mt-6'>
+          <p className='mt-6 font-montserrat'>
             A floral, solar and voluptuous interpretation composed by Olivier
             Polge, Perfumer-Creator for the House of CHANEL.
           </p>
 
-          <p className='mt-6 text-4xl font-extrabold'>
+          <p className='mt-6 text-4xl font-extrabold text-green-700'>
             $149.99{" "}
-            <span className='text-lg font-normal line-through'> $169.99</span>
+            <span className='ml-6 text-lg font-normal text-black line-through'>
+              {" "}
+              $169.99
+            </span>
           </p>
-          <button className='mt-4 w-full rounded-md border-2 bg-green-800 p-2 text-white'>
+          <button className='mt-8 w-full rounded-md border-2 bg-green-700 px-10 py-2 text-white hover:bg-green-900'>
             🛒Add to Cart
           </button>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,12 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: "'Montserrat', sans-serif",
+        fraunces: "'Fraunces', serif",
+      },
+    },
   },
   plugins: [],
 };

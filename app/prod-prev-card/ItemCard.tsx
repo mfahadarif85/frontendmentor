78de7a9aaaf022bf18c393f1f0bdd70ad4f70fdd
mfahadarif85 <mfahadarif@gmail.com>
2023-03-05 22:38:13 +0500

    prod-prev-card: improved styling

--- app/prod-prev-card/ItemCard.tsx
+++ app/prod-prev-card/ItemCard.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 type Product = {
   category: string;
   name: string;
@@ -20,29 +22,29 @@ const ItemCard = ({
   return (
     <>
       <div className='flex w-1/2 flex-col overflow-hidden rounded-lg bg-white shadow-lg md:flex-row'>
-        <div className='basis-1/2'>
+        <div className='w-1/2'>
           <picture>
             <img src={imageDesktop} alt='product' />
             <img src={imageMobile} alt='product' className='hidden' />
           </picture>
         </div>
-        <div className='mx-6 basis-1/2'>
-          <h3 className='mt-8 text-xl uppercase tracking-[5px] text-gray-400'>
+        <div className='flex flex-col space-y-6 p-6 md:w-1/2 md:p-8 md:pt-12'>
+          <h3 className='font-montserrat text-sm uppercase tracking-widest text-gray-400'>
             {category}
           </h3>
-          <h1 className='mt-6 font-fraunces text-5xl'>{name}</h1>
-          <p className='mt-6 font-montserrat'>{description}</p>
-          <div className='flex items-end'>
-            <p className='mt-6 text-4xl font-extrabold text-green-600'>
-              {price}{" "}
+          <h1 className='font-fraunces text-4xl'>{name}</h1>
+          <p className='font-montserrat text-lg text-gray-500'>{description}</p>
+          <div className='flex flex-row items-center space-x-10'>
+            <p className='font-fraunces text-4xl text-green-600'>
+              {`$${price}`}{" "}
             </p>
-            <p className='ml-6 text-lg font-normal text-black line-through'>
-              {" "}
-              {uvp}
-            </p>
+            <p className='text-black line-through'> {`$${uvp}`}</p>
           </div>
-          <button className='mt-8 w-full rounded-lg border-2 bg-green-600 px-10 py-4 font-bold text-white hover:bg-green-900'>
-            🛒Add to Cart
+          <button className='flex w-full items-center justify-center space-x-4 rounded-md bg-green-600 py-4 px-8 hover:bg-green-900'>
+            <Image alt='cart' src='/icon-cart.svg' width={16} height={16} />
+            <span className='font-montserrat text-lg font-bold text-white'>
+              Add to Cart
+            </span>
           </button>
         </div>
       </div>

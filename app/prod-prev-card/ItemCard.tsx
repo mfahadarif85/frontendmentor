fb580c42ee7d8ea8e45ede24ce096883735fb12f
mfahadarif85 <mfahadarif@gmail.com>
2023-03-27 16:05:26 +0500

    improved responsiveness

--- app/prod-prev-card/ItemCard.tsx
+++ app/prod-prev-card/ItemCard.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { ShoppingCartIcon } from "@heroicons/react/24/outline";
 
 type Product = {
   category: string;
@@ -20,35 +21,39 @@ const ItemCard = ({
   uvp,
 }: Product) => {
   return (
-    <>
-      <div className='flex w-1/2 flex-col overflow-hidden rounded-lg bg-white shadow-lg md:flex-row'>
-        <div className='w-1/2'>
-          <picture>
-            <img src={imageDesktop} alt='product' />
-            <img src={imageMobile} alt='product' className='hidden' />
-          </picture>
-        </div>
-        <div className='flex flex-col space-y-6 p-6 md:w-1/2 md:p-8 md:pt-12'>
-          <h3 className='font-montserrat text-sm uppercase tracking-widest text-gray-400'>
+    <div className='bg-cream mx-auto h-screen max-w-sm p-5 md:max-w-2xl'>
+      <div className='grid rounded-lg bg-white md:grid-cols-2'>
+        <picture>
+          <img
+            className='hidden rounded-l-lg object-cover md:block'
+            src={imageDesktop}
+            alt={name}
+          />
+          <img
+            className='rounded-t-lg object-cover md:hidden'
+            src={imageMobile}
+            alt={name}
+          />
+        </picture>
+        <div className='space-y-5 p-10'>
+          <h2 className='text-xs	uppercase tracking-[5px] text-gray-600'>
             {category}
-          </h3>
-          <h1 className='font-fraunces text-4xl'>{name}</h1>
-          <p className='font-montserrat text-lg text-gray-500'>{description}</p>
-          <div className='flex flex-row items-center space-x-10'>
-            <p className='font-fraunces text-4xl text-green-600'>
-              {`$${price}`}{" "}
+          </h2>
+          <h1 className='font-fraunces text-4xl text-[#1c232b]'>{name}</h1>
+          <p className='font-montserrat text-sm text-gray-600'>{description}</p>
+          <div className='relative'>
+            <p className='font-fraunces text-2xl	text-[#3c8067]'>{price}</p>
+            <p className='absolute top-2 left-28 font-montserrat text-xs text-gray-800 line-through'>
+              {uvp}
             </p>
-            <p className='text-black line-through'> {`$${uvp}`}</p>
           </div>
-          <button className='flex w-full items-center justify-center space-x-4 rounded-md bg-green-600 py-4 px-8 hover:bg-green-900'>
-            <Image alt='cart' src='/icon-cart.svg' width={16} height={16} />
-            <span className='font-montserrat text-lg font-bold text-white'>
-              Add to Cart
-            </span>
+          <button className='flex w-full items-center justify-center rounded-lg bg-[#3c8067] px-10 py-2 text-white hover:bg-[#1a4031]'>
+            <ShoppingCartIcon className='mr-2 h-4' />
+            Add to Cart
           </button>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
